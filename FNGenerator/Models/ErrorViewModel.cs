fc7a7b534ac88b7e7bd9d4e034ad6c6274cb8ab3
Ted Michalakes <tmichalakes@themxgroup.com>
2019-05-12 13:51:15 -0500

    Create Project With Test Framework

---
+++ FNGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FNGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
