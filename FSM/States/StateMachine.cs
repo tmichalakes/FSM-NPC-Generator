92408b5f98cf1852d10ad703a02c32ad0cda713b
Ted Michalakes <tmichalakes@gmail.com>
2019-07-17 19:32:23 -0600

    adding events

--- FSM/States/StateMachine.cs
+++ FSM/States/StateMachine.cs
@@ -4,8 +4,12 @@ using FSM.Triggers;
 
 namespace FSM.States {
     public abstract class StateMachine<S,T>{
-        State<S,T> CurrentState {get;set;}
-        List<State<S,T>> States {get;set;}
+        State<S,T> CurrentState { get; set; }
+        List<State<S,T>> States { get; set; }
+
+        public StateMachine<S,T>(EventSource events) {
+
+        }
 
         protected virtual void OnTriggerReceived(EventArgs e)
         {
